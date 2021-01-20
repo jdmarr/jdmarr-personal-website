c8e28aaff5fd43c9aac9a3bcf8dd1e96344ab2ac
Jordan Marr <jmarr.to@gmail.com>
2021-01-19 22:46:28 -0800

    Simple link checking

--- app.js
+++ app.js
@@ -2,10 +2,9 @@
 
 const express = require("express");
 const ejs = require("ejs");
+const https = require("https");
 
 const homeContent = "Hi, I'm Jordan. I'm a software engineer with a passion for autonomous vehicles and robotics. I work at Nvidia in Santa Clara, California, on the autonomous vehicles localization team. Not document translation, but estimation of a vehicle's position and orientation in the world.  It's exciting work, as we take all the noisy data from mass-market sensors, and refine it into an accurate estimate that is then used for vehicle control out on the road. It's given me extensive experience with estimation algorithms for camera, radar, and lidar, as well as allowing me to flex my muscles in C++, CUDA, and Python.";
-
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 
 const app = express();
 
@@ -20,8 +19,29 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", {
-    aboutParagraph: aboutContent
+  const qsetLink = "https://qset.ca";
+  let qsetLinkValid = false;
+  const starsLink = "https://starslab.ca/";
+  let starsLinkValid = false;
+  const nvidiaLink = "https://news.developer.nvidia.com/drive-labs-how-localization-helps-vehicles-find-their-way/";
+  let nvidiaLinkValid = false;
+
+  https.get(qsetLink, (qsetRes) => {
+    qsetLinkValid = qsetRes.statusCode === 200;
+    https.get(starsLink, (starsRes) => {
+      starsLinkValid = starsRes.statusCode === 200;
+      https.get(nvidiaLink, (nvidiaRes) => {
+        nvidiaLinkValid = nvidiaRes.statusCode === 200;
+        res.render("about", {
+          qsetLinkValid: qsetLinkValid,
+          qsetLink: qsetLink,
+          starsLinkValid: starsLinkValid,
+          starsLink: starsLink,
+          nvidiaLinkValid: nvidiaLinkValid,
+          nvidiaLink: nvidiaLink
+        });
+      });
+    });
   });
 });
 
